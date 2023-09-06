329e7277bc2dbad9fd9f3baed796072625be0c17
Sunset-Zen <142633964+Sunset-Zen@users.noreply.github.com>
2023-09-06 07:36:43 -0500

    Source code update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ string_board = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
 player1_f, player2_f, cpu_f = False, False, False
 turn_array = ['Heads', 'Tails']
 coin_flip = random.randint(0, 1)
-player1_symbol, cpu_symbol = 'p', 'c'
+player1_symbol, player2_symbol, cpu_symbol = 'p1', 'p2', 'c'
 
 # ( Functions )
 def display_board():
@@ -46,6 +46,56 @@ def re_roll(a, b):
     else:
         temp_b = random.randint(0, 6)
         re_roll(a, temp_b)
+
+def board_check(a_symbol, b_symbol):
+
+    # ( vertical )
+    for i in range(6):
+        a_count = 0
+        b_count = 0
+        for j in range(5):
+            if a_count == 4 or b_count == 4:
+                print('\nConnect 4 !!!!\n')
+                break
+            if board[i][j] == 1:
+                if string_board[i][j] == a_symbol:
+                    a_count += 1
+                    if i <= 2:
+                        if string_board[i][j+1] == a_symbol or string_board[i+1][j] == a_symbol:
+                            a_count += 1
+                        else:
+                            a_count = 1
+                    if i > 2:
+                        if string_board[i][j-1] == a_symbol or string_board[i-1][j] == a_symbol:
+                            a_count += 1
+                        else:
+                            a_count = 1
+                if string_board[i][j] == b_symbol:
+                    b_count += 1
+                    if i <= 2:
+                        if string_board[i][j+1] == b_symbol or string_board[i+1][j] == b_symbol:
+                            b_count += 1
+                        else:
+                            b_count = 1
+                    if i > 2:
+                        if string_board[i][j-1] == b_symbol or string_board[i-1][j] == b_symbol:
+                            b_count += 1
+                        else:
+                            b_count = 1
+
+
+    # ( horizontal )
+    # for i in range(len(board)):
+    #     for j in range(0, 6):
+    #         if string_board[i][j] == a_symbol and string_board[i][j+1] == a_symbol and string_board[i][j+2] == a_symbol and string_board[i][j+3] == a_symbol:
+    #             a_count += 4
+    #             print('\nConnect 4 !!!\n')
+    #         elif string_board[i][j] == b_symbol and string_board[i][j+1] == b_symbol and string_board[i][j+2] == b_symbol and string_board[i][j+3] == b_symbol:
+    #             b_count += 4
+    #             print('\nConnect 4 !!!\n')
+
+    # ( diagonal )
+
 
 print('=================================================' * 2)
 print('\nWelcome To Connect-4')
@@ -109,21 +159,28 @@ if player1_f:
         # ( Player 1 Input )
         player_input = input('\nPlayer 1 please enter location:\t')
         if board[row_counter][int(player_input)] == 1:
-            board[row_counter-1][int(player_input)] = 1
-            string_board[row_counter-1][int(player_input)] = player1_symbol
+            # make this linear vertical traversal ( stack )
+            for i in range(6):
+                if board[i][int(player_input)] == 0 and board[i+1][int(player_input)] == 1:
+                    board[i][int(player_input)] = 1
+                    string_board[i][int(player_input)] = player1_symbol
         else:
             board[row_counter][int(player_input)] = 1
             string_board[row_counter][int(player_input)] = player1_symbol
 
         # ( Update && Display Board )
         display_board()
+        board_check(player1_symbol, cpu_symbol)
         print('(row_count): ' + str(row_counter))
 
         # ( Generate CPU Input )
         cpu_input = random.randint(0, 6)
         if board[row_counter][cpu_input] == 1:
-            board[row_counter - 1][cpu_input] = 1
-            string_board[row_counter-1][cpu_input] = cpu_symbol
+            # make this linear vertical traversal ( stack )
+            for i in range(6):
+                if board[i][cpu_input] == 0 and board[i+1][cpu_input] == 1:
+                    board[i][cpu_input] = 1
+                    string_board[i][cpu_input] = cpu_symbol
         else:
             board[row_counter][cpu_input] = 1
             string_board[row_counter][cpu_input] = cpu_symbol
@@ -131,8 +188,8 @@ if player1_f:
 
         # ( Update && Display Board )
         display_board()
+        board_check(player1_symbol, cpu_symbol)
         print('(row_count): ' + str(row_counter))
-
 
 if cpu_f:
     row_counter = 5
@@ -152,8 +209,16 @@ if cpu_f:
         # ( Generate CPU Input )
         cpu_input = random.randint(0, 6)
         if board[row_counter][cpu_input] == 1:
-            board[row_counter - 1][cpu_input] = 1
-            string_board[row_counter - 1][cpu_input] = cpu_symbol
+            # make this linear vertical traversal ( stack )
+            for i in range(6):
+                if board[i][cpu_input] == 0 and board[i+1][cpu_input] == 1:
+                    board[i][cpu_input] = 1
+                    string_board[i][cpu_input] = cpu_symbol
+            #     else:
+            #         # ( re-roll )
+            #         cpu_input = random.randint(0, 6)
+            # board[row_counter - 1][cpu_input] = 1
+            # string_board[row_counter - 1][cpu_input] = cpu_symbol
         else:
             board[row_counter][cpu_input] = 1
             string_board[row_counter][cpu_input] = cpu_symbol
@@ -161,19 +226,24 @@ if cpu_f:
 
         # ( Update && Display Board )
         display_board()
+        board_check(player1_symbol, cpu_symbol)
         print('(row_count): ' + str(row_counter))
 
         # ( Player 1 Input )
         player_input = input('\nPlayer 1 please enter location:\t')
         if board[row_counter][int(player_input)] == 1:
-            board[row_counter - 1][int(player_input)] = 1
-            string_board[row_counter - 1][int(player_input)] = player1_symbol
+            # make this linear vertical traversal ( stack )
+            for i in range(6):
+                if board[i][int(player_input)] == 0 and board[i+1][int(player_input)] == 1:
+                    board[i][int(player_input)] = 1
+                    string_board[i][int(player_input)] = player1_symbol
         else:
             board[row_counter][int(player_input)] = 1
             string_board[row_counter][int(player_input)] = player1_symbol
 
         # ( Update && Display Board )
         display_board()
+        board_check(player1_symbol, cpu_symbol)
         print('(row_count): ' + str(row_counter))
 
 print('=================================================' * 2)
